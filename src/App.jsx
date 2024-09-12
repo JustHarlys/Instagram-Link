f1bc46b66b7fcf49efd15df54f57233e7dad089c
JustHarlys <harlysmc@gmail.com>
2024-09-11 21:40:50 -0400

    Terminando el link de Instagram

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Presentation from './Components/Presentation'
 import Button from './Components/Button'
-import Blog from './Components/Blog'
 import data from '../data'
 import { nanoid } from 'nanoid'
 
@@ -17,22 +16,29 @@ function App() {
     <Presentation/>
 
 
-      <p className='content-text'>Portfolio</p>
-    <div className='content-container'>
-      <a href="https://harlys-portfolio.vercel.app/" target='_blank'><p className='content-text'>Portfolio</p></a>
-    </div>
+    <section>
 
-      <p className='content-text'>CV (Coming soon)</p>
-    <div className='content-container'>
-    <a href=""><p className='content-text'>Download my CV</p></a>
-    </div>
+    <button className='button-container'>
+      <a href="https://harlys-portfolio.vercel.app/" target='_blank'><p className='content-text'>Portfolio</p></a>
+    </button>
+
+    <button className='button-container'>
+    <a href=""><p className='content-text'>Download my CV <br /> (Coming soon)</p></a>
+    </button>
+
+    <button className="button-container blog">
+        <p className="content-text">Blog (Coming soon)</p>
+    </button>
 
     <p className='content-text'>Social Media</p>
     <div className='social-media-container'>
       {social}
     </div>
 
-    <Blog />
+    </section>
+
+
+
 
 
     <footer className='content-text'>Created and Developed by Harlys Almanzar, 2024</footer>
